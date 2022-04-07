387618e2cb91406c130852c975c8f39b303e3d50
Jaopatk <jaopatk@gmail.com>
2022-04-07 21:39:41 +0700

    update some animated
    update fullpage and TweenMax

--- fullpages.js
+++ fullpages.js
@@ -1,7 +1,32 @@
 new fullpage('#fullpage', {
-	autoScrolling:true,
+	autoScrolling: true,
 	verticalCentered: true,
-	anchors:['Home', 'About','Contract']
+	anchors: ['Home', 'About', 'Contract'],
+	onLeave: function (origin, destination, direction) {
+		const section = destination.item;
+		const title = section.querySelector("h1");
+		const ptext = section.querySelector("p");
+		const tl = new TimelineMax({
+			delay: 0.5
+		});
+		tl.fromTo(title, 0.5, {
+			y: "50",
+			opacity: 0
+		}, {
+			y: "0",
+			opacity: 1
+		}, {
+			ease: "bounce"
+		});
+		tl.fromTo(ptext, 0.5, {
+			y: "50",
+			opacity: 0
+		}, {
+			y: "0",
+			opacity: 1
+		});
 
-	
+
+	}
+
 });
